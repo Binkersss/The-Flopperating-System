d3354e6f08e75095614b79ee4a6d544305b3d9b8
amar454 <aaamargml@gmail.com>
2025-12-25 12:32:59 -0800

    fix stupid formatting in turnstile

--- task/sync/turnstile.h
+++ task/sync/turnstile.h
@@ -9,28 +9,28 @@ typedef struct turnstile {
     spinlock_t wait_lock;
 } turnstile_t;
 
-void turnstile_init(turnstile_t* t) {
-    mutex_init(&t->lock, "turnstile");
-    t->wait_queue.head = NULL;
-    t->wait_queue.tail = NULL;
-    t->wait_queue.count = 0;
-    spinlock_init(&t->wait_queue.lock);
-    spinlock_init(&t->wait_lock);
+void turnstile_init(turnstile_t* turnstile) {
+    mutex_init(&turnstile->lock, "turnstile");
+    turnstile->wait_queue.head = NULL;
+    turnstile->wait_queue.tail = NULL;
+    turnstile->wait_queue.count = 0;
+    spinlock_init(&turnstile->wait_queue.lock);
+    spinlock_init(&turnstile->wait_lock);
 }
 
-void turnstile_enter(turnstile_t* t, process_t* owner) {
-    mutex_lock(&t->lock, owner);
+void turnstile_enter(turnstile_t* turnstile, process_t* owner) {
+    mutex_lock(&turnstile->lock, owner);
 }
 
-void turnstile_exit(turnstile_t* t) {
-    mutex_unlock(&t->lock);
+void turnstile_exit(turnstile_t* turnstile) {
+    mutex_unlock(&turnstile->lock);
 }
 
-void turnstile_wait(turnstile_t* t) {
+void turnstile_wait(turnstile_t* turnstile) {
     thread_t* cur = current_thread;
-    spinlock(&t->wait_lock);
+    spinlock(&turnstile->wait_lock);
     thread_t* prev = NULL;
-    thread_t* iter = t->wait_queue.head;
+    thread_t* iter = turnstile->wait_queue.head;
 
     while (iter && iter->priority.effective >= cur->priority.effective) {
         prev = iter;
@@ -38,27 +38,29 @@ void turnstile_wait(turnstile_t* t) {
     }
 
     if (!prev) {
-        cur->next = t->wait_queue.head;
-        t->wait_queue.head = cur;
-        if (!t->wait_queue.tail)
-            t->wait_queue.tail = cur;
+        cur->next = turnstile->wait_queue.head;
+        turnstile->wait_queue.head = cur;
+        if (!turnstile->wait_queue.tail) {
+            turnstile->wait_queue.tail = cur;
+        }
     } else {
         cur->next = prev->next;
         prev->next = cur;
-        if (!cur->next)
-            t->wait_queue.tail = cur;
+        if (!cur->next) {
+            turnstile->wait_queue.tail = cur;
+        }
     }
 
-    t->wait_queue.count++;
-    spinlock_unlock(&t->wait_lock, true);
+    turnstile->wait_queue.count++;
+    spinlock_unlock(&turnstile->wait_lock, true);
     sched_block();
 }
 
-void turnstile_wake(turnstile_t* t) {
-    spinlock(&t->wait_lock);
-    thread_t* highest = t->wait_queue.head;
+void turnstile_wake(turnstile_t* turnstile) {
+    spinlock(&turnstile->wait_lock);
+    thread_t* highest = turnstile->wait_queue.head;
     thread_t* prev = NULL;
-    thread_t* iter = t->wait_queue.head;
+    thread_t* iter = turnstile->wait_queue.head;
     thread_t* prev_high = NULL;
 
     while (iter) {
@@ -71,33 +73,35 @@ void turnstile_wake(turnstile_t* t) {
     }
 
     if (highest) {
-        if (prev_high)
+        if (prev_high) {
             prev_high->next = highest->next;
-        else
-            t->wait_queue.head = highest->next;
+        } else {
+            turnstile->wait_queue.head = highest->next;
+        }
 
-        if (highest == t->wait_queue.tail)
-            t->wait_queue.tail = prev_high;
+        if (highest == turnstile->wait_queue.tail) {
+            turnstile->wait_queue.tail = prev_high;
+        }
 
         highest->next = NULL;
-        t->wait_queue.count--;
+        turnstile->wait_queue.count--;
         sched_unblock(highest);
     }
 
-    spinlock_unlock(&t->wait_lock, true);
+    spinlock_unlock(&turnstile->wait_lock, true);
 }
 
-void turnstile_wake_all(turnstile_t* t) {
-    spinlock(&t->wait_lock);
-    while (t->wait_queue.head) {
-        turnstile_wake(t);
+void turnstile_wake_all(turnstile_t* turnstile) {
+    spinlock(&turnstile->wait_lock);
+    while (turnstile->wait_queue.head) {
+        turnstile_wake(turnstile);
     }
-    spinlock_unlock(&t->wait_lock, true);
+    spinlock_unlock(&turnstile->wait_lock, true);
 }
 
-int turnstile_empty(turnstile_t* t) {
-    spinlock(&t->wait_lock);
-    int empty = t->wait_queue.count == 0;
-    spinlock_unlock(&t->wait_lock, true);
+int turnstile_empty(turnstile_t* turnstile) {
+    spinlock(&turnstile->wait_lock);
+    int empty = turnstile->wait_queue.count == 0;
+    spinlock_unlock(&turnstile->wait_lock, true);
     return empty;
 }
